cb2311bc2637d0d8181bd564e9959b4dffe14f58
Derek-Emsbach <demsbach92@gmail.com>
2024-02-09 21:07:11 -0700

    App.js testing

--- App.js
+++ App.js
@@ -1,21 +1,22 @@
-import { StatusBar } from 'expo-status-bar';
-import { SafeAreaView } from 'react-native';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { SafeAreaView } from "react-native";
+import { StyleSheet, Text } from "react-native";
 
 export default function App() {
-  return (
-    <SafeAreaView style={styles.container}>
-      <Text>Done With It App</Text>
-      <StatusBar style="auto" />
-    </SafeAreaView>
-  );
+	const handlePress = () => console.log("Text Pressed");
+	return (
+		<SafeAreaView style={styles.container}>
+			<Text onPress={handlePress}>Done With It App</Text>
+			<StatusBar style="auto" />
+		</SafeAreaView>
+	);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+	container: {
+		flex: 1,
+		backgroundColor: "#fff",
+		alignItems: "center",
+		justifyContent: "center",
+	},
 });
